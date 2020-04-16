f283509bb3828af2ac09d1c5b3d61f631136aae8
Ricardo Balbinot <ricardo.balbinot@ciss.com.br>
2020-04-16 19:36:39 -0300

    Rotas de criação e listagem

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  private sumTransactions(type: string): number {
+    return this.transactions
+      .filter(transaction => transaction.type === type)
+      .reduce(
+        (total, filteredTransaction) => total + filteredTransaction.value,
+        0,
+      );
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.sumTransactions('income');
+
+    const outcome = this.sumTransactions('outcome');
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
